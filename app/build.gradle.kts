b860674d8b531c6016adaaa870c80fc7519075d7
Lorenzo Gerosa <lorenzo.gerosa@hrm.group>
2025-01-05 19:29:49 +0100

    Room setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,14 +2,15 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
 }
 
 android {
-    namespace = "com.example.mangiaebasta"
+    namespace = "com.example.roomexample"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.mangiaebasta"
+        applicationId = "com.example.roomexample"
         minSdk = 34
         //noinspection OldTargetApi
         targetSdk = 34
@@ -50,13 +51,30 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-//    NAVBAR
-    implementation (libs.androidx.navigation.compose)
+    implementation(libs.androidx.room.runtime)
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation(libs.androidx.room.ktx)
+    // optional - RxJava2 support for Room
+    implementation(libs.androidx.room.rxjava2)
+    // optional - RxJava3 support for Room
+    implementation(libs.androidx.room.rxjava3)
+    // optional - Guava support for Room, including Optional and ListenableFuture
+    implementation(libs.androidx.room.guava)
+    // optional - Paging 3 Integration
+    implementation(libs.androidx.room.paging)
     testImplementation(libs.junit)
+    // optional - Test helpers
+    testImplementation(libs.androidx.room.testing)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    // If this project uses any Kotlin source, use Kotlin Symbol Processing (KSP)
+    // See Add the KSP plugin to your project
+    ksp(libs.androidx.room.compiler)
+    // If this project only uses Java source, use the Java annotationProcessor
+    // No additional plugins are necessary
+    annotationProcessor(libs.androidx.room.compiler)
 }
